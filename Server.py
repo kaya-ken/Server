88f2f14636ced3281461a52091b77c8f33267474
kaya-ken <ctwd0123@mail4.doshisha.ac.jp>
2019-07-23 16:44:42 +0900

    Initial commit

---
+++ Server.py
@@ -0,0 +1,24 @@
+# socket サーバを作成
+
+import socket
+
+# AF = IPv4 という意味
+# TCP/IP の場合は、SOCK_STREAM を使う
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # IPアドレスとポートを指定
+    s.bind(('0.0.0.0', 8084))
+    # 1 接続
+    s.listen(1)
+    # connection するまで待つ
+    while True:
+        # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                # データを受け取る
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('data : {}, addr: {}'.format(data, addr))
+                # クライアントにデータを返す(b -> byte でないといけない)
+                conn.sendall(b'Received: ' + data)
