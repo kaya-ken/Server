91dda2b7a562b6a11c99fdd282db57bf2e8d13af
NB29979 <ctwc0162@mail4.doshisha.ac.jp>
2019-11-09 15:17:29 +0900

    サーバとして動作するようコードの整理

--- Server.py
+++ Server.py
@@ -12,30 +12,19 @@ if __name__ == '__main__':
 
     proxy = 'http://lab-12:Slpl-201@proxy.doshisha.ac.jp:8080'
     slackClient = SlackAPI.SlackClient(slack_token, proxy)
-    channels = slackClient.list_channels()
-    users = slackClient.list_users()
+    channels = slackClient.list_up_channels()
+    users = slackClient.list_up_users()
 
-    if users:
-        for u in users:
-            if not u['deleted']:
-                user2id[u['name']] = u['id']
-    else:
-        print("Unable to authenticate.")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', 8084))
 
-    # img = open('QR/岸田 優輝.png', 'rb')
-    slackClient.send_message(user2id['ctwc0162'], 'Images/logo.png')
-    slackClient.send_image(user2id['ctwc0162'], 'Images/logo.png')
-
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #     s.bind(('0.0.0.0', 8084))
-    #
-    #     s.listen(1)
-    #     while True:
-    #         conn, addr = s.accept()
-    #         with conn:
-    #             while True:
-    #                 data = conn.recv(1024)
-    #                 if not data:
-    #                     break
-    #                 print('data : {}, addr: {}'.format(data, addr))
-    #                 conn.sendall(b'Received: ' + data)
+        s.listen(1)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print('data : {}, addr: {}'.format(data, addr))
+                    conn.sendall(b'Received: ' + data)
