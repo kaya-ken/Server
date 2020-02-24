71bfbabe6cb328f03eda0e3dcd34c534f1896d48
NB29979 <12462512+NB29979@users.noreply.github.com>
2020-02-24 18:54:10 +0900

    PandasではなくSQLite3を使うように

--- PurchaseHistoryLogger.py
+++ PurchaseHistoryLogger.py
@@ -1,17 +1,20 @@
-import pandas as pd
 import datetime
-
-from DataFrame import DataFrame
+import sqlite3
 
 
 class PurchaseHistoryLogger(object):
     def log_purchase(self, _user_info):
         dt_now = datetime.datetime.now()
         str_time = dt_now.strftime('%Y/%m/%d %H:%M:%S')
-        tmp_series = pd.Series([_user_info.slack_id, _user_info.product_id, str_time],
-                               index=DataFrame.purchase_history_df.columns)
-        DataFrame.purchase_history_df = DataFrame.purchase_history_df \
-            .append(tmp_series, ignore_index=True)
 
-    def debug(self):
-        print(DataFrame.purchase_history_df.head())
+        connection = sqlite3.connect('purchase_history_db.sqlite3')
+        c = connection.cursor()
+
+        try:
+            c.execute("INSERT INTO purchase_history VALUES (? , ?, ?)",
+                              (_user_info.slack_id, _user_info.product_id, str_time))
+        except sqlite3.Error as e:
+            print('sqlite3 error:', e.args[0])
+
+        connection.commit()
+        connection.close()
--- Server.py
+++ Server.py
@@ -2,22 +2,16 @@
 
 import socket
 import os
-import SlackAPI
 import json
+import sqlite3
 
 import UserInfo
 from PurchaseHistoryLogger import PurchaseHistoryLogger
 
 
 if __name__ == '__main__':
-    user2id = {}
-    slack_token = os.getenv('SLACK_TOKEN')
     logger = PurchaseHistoryLogger()
 
-    proxy = 'MY_PROXY'
-    slackClient = SlackAPI.SlackClient(slack_token, proxy)
-    # channels = slackClient.list_up_channels()
-    # users = slackClient.list_up_users()
 
     # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #     s.bind(('0.0.0.0', 8084))
@@ -38,7 +32,13 @@ if __name__ == '__main__':
     receivedUserInfo = UserInfo.ReceivedData(slack_id='ctwc0162', product_id='hOgEpRoDuCt')
     json_data = json.dumps(receivedUserInfo, default=lambda o: o.__dict__, indent=4)
 
-    decoded_team = UserInfo.ReceivedData(**json.loads(json_data))
-    logger.log_purchase(decoded_team)
-    logger.debug()
+    decoded_user_info = UserInfo.ReceivedData(**json.loads(json_data))
+    logger.log_purchase(decoded_user_info)
 
+    conn = sqlite3.connect('purchase_history_db.sqlite3')
+    c = conn.cursor()
+    c.execute('SELECT * FROM purchase_history')
+    print(c.fetchone())
+    c.execute('SELECT * FROM product')
+    print(c.fetchone())
+    conn.close()
