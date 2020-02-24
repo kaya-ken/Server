0c2e7b18dffd748ff3701535ce799b026b66b282
NB29979 <12462512+NB29979@users.noreply.github.com>
2020-02-24 21:02:04 +0900

    DB上で経過日数の計算をするための準備

--- PurchaseHistoryLogger.py
+++ PurchaseHistoryLogger.py
@@ -5,13 +5,13 @@ import sqlite3
 class PurchaseHistoryLogger(object):
     def log_purchase(self, _user_info):
         dt_now = datetime.datetime.now()
-        str_time = dt_now.strftime('%Y/%m/%d %H:%M:%S')
+        str_time = dt_now.strftime('%Y-%m-%d %H:%M:%S')
 
         connection = sqlite3.connect('purchase_history_db.sqlite3')
         c = connection.cursor()
 
         try:
-            c.execute("INSERT INTO purchase_history VALUES (? , ?, ?)",
+            c.execute("INSERT INTO purchase_history VALUES (?, ?, ?)",
                               (_user_info.slack_id, _user_info.product_id, str_time))
         except sqlite3.Error as e:
             print('sqlite3 error:', e.args[0])
